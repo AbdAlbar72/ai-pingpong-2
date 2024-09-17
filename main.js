f82e7bce8362f089642dec20c643baea516a5528
AbdAlbar72 <131976632+AbdAlbar72@users.noreply.github.com>
2024-09-17 20:19:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function setup() {
+    
+    let canvas = createCanvas(800, 600);
+    canvas.parent('canvas-container');
+    
+}
+
+
+function draw() {
+
+    background(0);
+
+
+}
+
+function startGame() {
+
+    document.getElementById('game-status').innerText = "Game Started!";
+}
+
+
